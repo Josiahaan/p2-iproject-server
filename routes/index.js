496ce60bc062c9d54706ab0e4a6958a5951b9191
Jonathan Sebastian <j.shn50@gmail.com>
2022-03-23 09:41:44 +0700

    revisi fitur train

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,12 @@ const routerCartItem = require("./cartitem")
 const routerProduct = require("./product")
 const routerShop = require('./shop')
 const routerPulsa = require('./pulsa')
-const routerTrain = require('./train')
 
 router.use(routerUser)
 router.use("/cartitem", routerCartItem)
 router.use("/product", routerProduct)
 router.use("/shop", routerShop)
 router.use("/pulsa", routerPulsa)
-router.use("/train", routerTrain)
 
 
 module.exports = router
